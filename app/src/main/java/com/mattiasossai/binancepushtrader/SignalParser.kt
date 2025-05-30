336d211dc6574f11253c94139cb6eb8a80d4e983
mattiasossai <mattiasossai@yahoo.com>
2025-05-30 07:05:58 +0200

    Update SignalParser.kt

--- app/src/main/java/com/mattiasossai/binancepushtrader/SignalParser.kt
+++ app/src/main/java/com/mattiasossai/binancepushtrader/SignalParser.kt
@@ -1,5 +1,16 @@
 package com.mattiasossai.binancepushtrader
 
 object SignalParser {
-    // TODO: Implementiere Parsing von Binance-Benachrichtigungen
+    fun parse(text: String): Signal? {
+        // Beispiel: "Order filled: BTCUSDT BUY 0.1"
+        val parts = text.split("\\s+".toRegex())
+        return try {
+            val symbol = parts[2]
+            val side = parts[3]
+            val qty = parts[4].toDouble()
+            Signal(symbol, side, qty)
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
